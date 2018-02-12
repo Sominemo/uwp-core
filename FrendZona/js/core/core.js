804be8dd3aaa7799936a995469d2dffe713eb5a3
Sominemo <dilongzhenya@gmail.com>
2018-02-12 20:35:08 +0200

    Getting app version

--- FrendZona/js/core/core.js
+++ FrendZona/js/core/core.js
@@ -3,7 +3,9 @@
         name: Windows.ApplicationModel.Package.current.displayName,
         publisher: Windows.ApplicationModel.Package.current.publisherDisplayName,
         about: Windows.ApplicationModel.Package.current.description,
-        installed: Windows.ApplicationModel.Package.current.installedDate
+        installed: Windows.ApplicationModel.Package.current.installedDate,
+        version: parseFloat(Windows.ApplicationModel.Package.current.id.version.major + '.' + Windows.ApplicationModel.Package.current.id.version.minor),
+        build: Windows.ApplicationModel.Package.current.id.version.build
     },
 
     status: {
