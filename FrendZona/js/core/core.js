a4247ac01939ae9cf056cb352147562b4776b429
Sominemo <admin@sominemo.com>
2018-02-12 20:27:07 +0200

    Basic declarations based on Windows API

--- FrendZona/js/core/core.js
+++ FrendZona/js/core/core.js
@@ -1,1 +1,12 @@
-﻿
+﻿var app = {
+    info: {
+        name: Windows.ApplicationModel.Package.current.displayName,
+        publisher: Windows.ApplicationModel.Package.current.publisherDisplayName,
+        about: Windows.ApplicationModel.Package.current.description,
+        installed: Windows.ApplicationModel.Package.current.installedDate
+    },
+
+    status: {
+        debug: Windows.ApplicationModel.Package.current.isDevelopmentMode
+    }
+};
