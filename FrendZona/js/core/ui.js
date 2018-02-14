11dce3059c67b3e40fb95f83f865c1347e9283ec
Sominemo <dilongzhenya@gmail.com>
2018-02-14 17:42:20 +0200

    Theme change preparation

--- FrendZona/js/core/ui.js
+++ FrendZona/js/core/ui.js
@@ -1,1 +1,14 @@
-﻿
+﻿var ui = {
+    theme: {
+        prop: {
+            mainColor: "#34BFB0"
+        },
+        apply: function () {
+            //let titleBar = Windows.UI.ApplicationView.GetForCurrentView().TitleBar;
+            //titleBar.BackgroundColor = Colors.Black;
+            //titleBar.ForegroundColor = Colors.White;
+            //titleBar.ButtonBackgroundColor = Colors.Black;
+            //titleBar.ButtonForegroundColor = Colors.White;
+        }
+    }
+}
