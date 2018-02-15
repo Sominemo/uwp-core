77c898d9df120238d9648fe4100c9ef5f18fd42e
Sominemo <dilongzhenya@gmail.com>
2018-02-15 23:59:11 +0200

    Removed trash

--- FrendZona/js/core/ui.js
+++ FrendZona/js/core/ui.js
@@ -4,11 +4,7 @@
             mainColor: "#34BFB0"
         },
         apply: function () {
-            //let titleBar = Windows.UI.ApplicationView.GetForCurrentView().TitleBar;
-            //titleBar.BackgroundColor = Colors.Black;
-            //titleBar.ForegroundColor = Colors.White;
-            //titleBar.ButtonBackgroundColor = Colors.Black;
-            //titleBar.ButtonForegroundColor = Colors.White;
+
         }
     }
 }
