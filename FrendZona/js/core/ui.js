9c7d85d10ca1f03fd5a95868d034435b7c3997e8
Sominemo <dilongzhenya@gmail.com>
2018-02-16 22:53:51 +0200

    theme.apply got an ability to change titlebar color (thx Igor Fedorov)

--- FrendZona/js/core/ui.js
+++ FrendZona/js/core/ui.js
@@ -1,9 +1,45 @@
 ﻿var ui = {
+    color: {
+        white: new UWPColor('#FFFFFF').rgba
+    },
     theme: {
         prop: {
-            mainColor: "#34BFB0"
+            mainColor: new UWPColor("#31b3a5").rgba,
+            mainLight: new UWPColor("#5aebdb").rgba,
+            mainLight1: new UWPColor("#73fdee").rgba
         },
         apply: function () {
+            let viewManagement = Windows.UI.ViewManagement;
+            ApplicationView = viewManagement.ApplicationView;
+            applicationView = ApplicationView.getForCurrentView();
+
+            let statusbar = viewManagement.StatusBar ? viewManagement.StatusBar.getForCurrentView() : null,
+                titlebar = applicationView.titleBar || {};
+
+            if (statusbar) {
+                statusbar.showAsync();
+                statusbar.backgroundOpacity = 1;
+                statusbar.foregroundColor = ui.color.white;
+            }
+
+            if (statusbar) {
+                statusbar["backgroundColor"] = ui.theme.prop.mainColor;
+            } else {
+                titlebar["foregroundColor"] = ui.color.white;
+                titlebar["backgroundColor"] = ui.theme.prop.mainColor;
+                titlebar["buttonForegroundColor"] = ui.color.white;
+                titlebar["buttonBackgroundColor"] = ui.theme.prop.mainColor;
+                titlebar["buttonHoverForegroundColor"] = ui.color.white;
+                titlebar["buttonHoverBackgroundColor"] = ui.theme.prop.mainLight;
+                titlebar["inactiveForegroundColor"] = ui.color.white;
+                titlebar["inactiveBackgroundColor"] = ui.theme.prop.mainColor;
+                titlebar["buttonInactiveForegroundColor"] = ui.color.white;
+                titlebar["buttonInactiveBackgroundColor"] = ui.theme.prop.mainColor;
+                titlebar["buttonInactiveHoverForegroundColor"] = ui.color.white;
+                titlebar["buttonInactiveHoverBackgroundColor"] = ui.theme.prop.mainLight;
+                titlebar["buttonPressedForegroundColor"] = ui.color.white;
+                titlebar["buttonPressedBackgroundColor"] = ui.theme.prop.mainLight1;
+            }
 
         }
     }
