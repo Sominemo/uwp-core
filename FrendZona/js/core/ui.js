cad934fe5a32ecd636f497f68c31d2340a26e0a2
Sominemo <dilongzhenya@gmail.com>
2018-02-18 22:40:15 +0200

    Sidebar fill, Responsible UI, Custom splash screen, registred events in onLoad Manager

--- FrendZona/js/core/ui.js
+++ FrendZona/js/core/ui.js
@@ -1,21 +1,59 @@
 ﻿var ui = {
     color: {
-        white: new UWPColor('#FFFFFF').rgba
+        white: new UWPColor("#FFFFFF").rgba
+    },
+    makeResponsible: function (a) {
+             document.getElementById("menu-control").addEventListener("click", function () { document.getElementById("sidebar").classList.toggle("opened") });
+             a();
+    },
+    fillSideBar: function (a) {
+        document.getElementById("sb-scroll").innerHTML = '<div id="sidebar-header-margin"></div>';
+        app.modules.list.forEach((e) => {
+            if (!e.inMenu) return;
+
+            let el = document.createElement("div");
+            el.classList.add("sidebar-e");
+            el.onclick = e.startFunc;
+            el.setAttribute("sb-element-id", el.id);
+
+            let i = document.createElement("icon");
+            i.innerHTML = e.icon;
+
+            let t = document.createElement("div");
+            t.classList.add("sb-e-text");
+            t.innerHTML = e.localized ? _(e.name) : e.name;
+
+            el.appendChild(i);
+            el.appendChild(t);
+            document.getElementById("sb-scroll").appendChild(el);
+        });
+        a();
     },
     theme: {
         prop: {
             mainColor: new UWPColor("#31b3a5").rgba,
-            mainLight: new UWPColor("#5aebdb").rgba,
-            mainLight1: new UWPColor("#73fdee").rgba
+            mainDark: new UWPColor("#31a094").rgba,
+            mainDark1: new UWPColor("#2e9186").rgba,
+            mainRealDark: new UWPColor("#1c6f66").rgba,
+            mainLight: new UWPColor("#49c6b8").rgba,
+            mainLight1: new UWPColor("#71d2c8").rgba,
+            mainRealLight: new UWPColor("#5aebdb").rgba,
+            mainRealLight1: new UWPColor("#73fdee").rgba
         },
         apply: function () {
+            // Setting fast variables
             let viewManagement = Windows.UI.ViewManagement;
             ApplicationView = viewManagement.ApplicationView;
             applicationView = ApplicationView.getForCurrentView();
 
+            // Setting own min sizes
+            applicationView.setPreferredMinSize({width: 360, height: 640});
+
+            // More fast variables, cause WinAPI namespaces are TOOOO long
             let statusbar = viewManagement.StatusBar ? viewManagement.StatusBar.getForCurrentView() : null,
                 titlebar = applicationView.titleBar || {};
 
+            // Making TB colored!
             if (statusbar) {
                 statusbar.showAsync();
                 statusbar.backgroundOpacity = 1;
@@ -23,8 +61,10 @@
             }
 
             if (statusbar) {
+                // If mobile
                 statusbar["backgroundColor"] = ui.theme.prop.mainColor;
             } else {
+                // If pc
                 titlebar["foregroundColor"] = ui.color.white;
                 titlebar["backgroundColor"] = ui.theme.prop.mainColor;
                 titlebar["buttonForegroundColor"] = ui.color.white;
@@ -44,3 +84,8 @@
         }
     }
 }
+
+
+// Make SB responsible task and fill menu
+loadRegister.registerNew({ name: 'responsible_ui', func: ui.makeResponsible });
+loadRegister.registerNew({ name: 'fill_sb', func: ui.fillSideBar });
